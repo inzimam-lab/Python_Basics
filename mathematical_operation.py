7e811aea3038e3b0fe1738779a006a4103007b8e
inzimam-lab <inzimam3859@gmail.com>
2025-03-08 18:27:57 +0100

    Add files via upload

---
+++ mathematical_operation.py
@@ -0,0 +1,28 @@
+
+
+a = int(input("enter your first number \n"))
+b= int(input("enter your second number \n"))
+c="I love Python"
+
+sum_total = a+b
+difference_total = a-b
+product_result = a*b
+quotient_result= b/a
+
+input("Press Enter for mathematical operations")
+
+
+print("a+b=", sum_total)
+input("Press Enter to continue")
+
+print("a-b=", difference_total)
+input("Press Enter to continue")
+
+print("a*b=", product_result)
+input("Press Enter to continue")
+
+print("b/a=", quotient_result)
+input("Press Enter to continue")
+
+print(c)
+
